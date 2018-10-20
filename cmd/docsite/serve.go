edff08afd08219967efffd8e24cda8f0c18c30f5
Quinn Slack <qslack@qslack.com>
2018-10-19 18:46:23 -0700

    remove Let's Encrypt, add TLS cert and key file support

--- cmd/docsite/serve.go
+++ cmd/docsite/serve.go
@@ -3,18 +3,18 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-
-	"golang.org/x/crypto/acme/autocert"
 )
 
 func init() {
 	flagSet := flag.NewFlagSet("serve", flag.ExitOnError)
 	var (
-		httpAddr       = flagSet.String("http", ":5080", "HTTP listen address for previewing")
-		autocertDomain = flagSet.String("autocert-domain", "", "enable TLS listener and autocert for this domain name")
+		httpAddr    = flagSet.String("http", ":5080", "HTTP listen address for previewing")
+		tlsCertPath = flagSet.String("tls-cert", "", "path to TLS certificate file")
+		tlsKeyPath  = flagSet.String("tls-key", "", "path to TLS key file")
 	)
 
 	handler := func(args []string) error {
@@ -38,14 +38,24 @@ func init() {
 		if err != nil {
 			return err
 		}
-		if *autocertDomain != "" {
-			log.Printf("# TLS listener enabled, Let's Encrypt domain %s", *autocertDomain)
-			m := &autocert.Manager{
-				Prompt:     autocert.AcceptTOS,
-				HostPolicy: autocert.HostWhitelist(*autocertDomain),
+		if *tlsCertPath != "" || *tlsKeyPath != "" {
+			log.Printf("# TLS listener enabled")
+			tlsCert, err := ioutil.ReadFile(*tlsCertPath)
+			if err != nil {
+				return err
 			}
-			l = tls.NewListener(l, m.TLSConfig())
-			handler = m.HTTPHandler(handler)
+			tlsKey, err := ioutil.ReadFile(*tlsKeyPath)
+			if err != nil {
+				return err
+			}
+			cert, err := tls.X509KeyPair(tlsCert, tlsKey)
+			if err != nil {
+				return err
+			}
+			l = tls.NewListener(l, &tls.Config{
+				NextProtos:   []string{"h2", "http/1.1"},
+				Certificates: []tls.Certificate{cert},
+			})
 		}
 		log.Printf("# Doc site is available at http://%s:%s", host, port)
 		return http.Serve(l, handler)
