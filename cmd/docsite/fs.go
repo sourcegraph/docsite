716f8863a723bd8c6c1e56982fde5ea8a7f92a1f
Quinn Slack <qslack@qslack.com>
2019-11-06 00:19:55 -0800

    add `docsite ls` command to list all documents in the docsite

---
+++ cmd/docsite/fs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+)
+
+func init() {
+	flagSet := flag.NewFlagSet("ls", flag.ExitOnError)
+	var (
+		contentVersion = flagSet.String("content-version", "", "version of content to check")
+	)
+
+	handler := func(args []string) error {
+		flagSet.Parse(args)
+		site, _, err := siteFromFlags()
+		if err != nil {
+			return err
+		}
+
+		ctx := context.Background()
+		pages, err := site.AllContentPages(ctx, *contentVersion)
+		if err != nil {
+			return err
+		}
+
+		for _, page := range pages {
+			fmt.Printf("%s\n", page.FilePath)
+		}
+		return nil
+	}
+
+	commands = append(commands, &command{
+		FlagSet:          flagSet,
+		ShortDescription: "list all documents",
+		LongDescription:  "The ls subcommand lists all documents.",
+		handler:          handler,
+	})
+}
