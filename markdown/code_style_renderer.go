f2d975168bdfcaa0dda9a6e39b6c84a7dbb2e271
Camden Cheek <camden@ccheek.com>
2021-03-24 15:52:59 -0600

    Fix rendering of plaintext blocks (#71)
    The logic for adding the language name to the class skipped fences where
    the language wasn't specified. Now, we treat those as plaintext.

--- markdown/code_style_renderer.go
+++ markdown/code_style_renderer.go
@@ -13,12 +13,15 @@ type CodeStyleRenderer struct {
 
 func (c CodeStyleRenderer) RenderNode(w io.Writer, node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
 	if node.Type == blackfriday.CodeBlock {
-		if lang := string(node.Info); lang != "" {
-			fmt.Fprintf(w, `<pre class="chroma %s">`, lang)
-			walkStatus := c.Renderer.RenderNode(w, node, entering)
-			fmt.Fprint(w, "</pre>")
-			return walkStatus
+		lang := string(node.Info)
+		if lang == "" {
+			lang = "plaintext"
 		}
+
+		fmt.Fprintf(w, `<pre class="chroma %s">`, lang)
+		walkStatus := c.Renderer.RenderNode(w, node, entering)
+		fmt.Fprint(w, "</pre>")
+		return walkStatus
 	}
 
 	return c.Renderer.RenderNode(w, node, entering)
